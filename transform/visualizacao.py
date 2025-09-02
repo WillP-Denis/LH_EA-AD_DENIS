8e03a473933e60dda1e8fd761a6cdb5323764223
DWDEnis <deniswillianpereira02@gmail.com>
2025-09-02 19:05:36 -0300

    Primeiras alteracoes

---
+++ transform/visualizacao.py
@@ -0,0 +1,14 @@
+# %%
+import pandas as pd
+# %%
+agencias = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/agencias.csv')
+clientes = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/clientes.csv')
+agencia_colab = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/colaborador_agencia.csv')
+colaboradores = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/colaboradores.csv')
+contas = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/contas.csv')
+propostas_cred = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/propostas_credito.csv')
+transacoes = pd.read_csv('C:/Users/denis/OneDrive/Área de Trabalho/Analises/LH_EA-AD_Denis/arquivos_csv/transacoes.csv')
+# %%
+
+
+# %%
